3f7588314cada190ef2789d281fb438f7c6887c5
ba3uk <ba3a2022@gmail.com>
2019-09-06 01:34:59 +0700

    broke a class into several subclasses

--- Lesson_1/Lesson_1/Program.cs
+++ Lesson_1/Lesson_1/Program.cs
@@ -4,10 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson_1
-{
-    class Program
-    {
+namespace Lesson_1_4
 
+{
+    class Vector2
+    {
+        public float X;
+        public float Y;
+    }
+
+    class Motor
+    {
+        public float MovementSpeed { get; private set; }
+        public Vector2 MovementDirection { get; private set; }
+    }
+
+    class Weapon
+    {
+        public string Name { get; private set; }
+
+        public int WeaponDamage { get; private set; }
+        public float WeaponCooldown { get; private set; }
+
+        public bool IsReloading()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Player
+    {
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }  
+        
+        public Weapon Weapon { get; private set; }
+
+        public Motor PlayerMotor { get; private set; }
+
+
+        public void Move()
+        {
+            //Do move
+        }
+
+        public void Attack()
+        {
+            if (!Weapon.IsReloading())
+            {
+                //attack
+            }
+        }
     }
 }
