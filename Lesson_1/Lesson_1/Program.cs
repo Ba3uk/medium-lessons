e9b737772174702ab0030b8b807e7c8c60854634
ba3uk <ba3a2022@gmail.com>
2019-09-06 01:26:20 +0700

    Complite

--- Lesson_1/Lesson_1/Program.cs
+++ Lesson_1/Lesson_1/Program.cs
@@ -4,10 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson_1
+namespace Lesson_1_3
 {
-    class Program
+    class Bag
     {
+        public List<Item> Items;
+        public int MaxWeidth { private set; get; }
 
+        public void AddItem(string name, int count)
+        {
+            int currentWeidth = Items.Sum(item => item.Count);
+            Item targetItem = Items.FirstOrDefault(item => item.Name == name);
+
+            if (targetItem == null)
+                throw new InvalidOperationException();
+
+            if (currentWeidth + count > MaxWeidth)
+                throw new InvalidOperationException();
+
+            targetItem.Count += count;
+        }
+    }
+
+    class Item
+    {
+        private int _count;
+
+        public int Count
+        {
+            set
+            {
+                _count = value;
+                if (_count < 0)
+                    _count = 0;
+            }
+
+            get { return _count; }
+        }
+        public string Name { private set; get; }
     }
 }
