bf2438e217643e7b4cee7a0d09cb23d849fe19ac
ba3uk <ba3a2022@gmail.com>
2019-09-25 07:29:37 +0700

    Created a wrapper for all interfaces

--- Lesson_1/Lesson_1/Program.cs
+++ Lesson_1/Lesson_1/Program.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lesson_2_4
 {
+
     public interface ReceivingInformation
     {
         string GetInformation();
@@ -16,6 +18,7 @@ namespace Lesson_2_4
     {
         string SetText { set; }
         string GetStreamProcessing();
+        bool CanReturnStream { get; }
     }
 
     public interface VisualDisplay
@@ -23,32 +26,57 @@ namespace Lesson_2_4
         void PrinteInfo(string message);
     }
 
+
     public class FolderTXTReceivingInformation : ReceivingInformation
     {
-        private string[] textFileName;
+        private List<string> textFileName;
+
         public FolderTXTReceivingInformation(string path, string expansion)
         {
-            textFileName = Directory.GetFiles(path, expansion);
-            foreach(var fileName in textFileName)
-            {
-                Console.WriteLine(fileName);
-            }
+            textFileName = Directory.GetFiles(path, expansion).ToList();
         }
+
         public string GetInformation()
         {
-            return ";";
-        }
-    }
+            string resultLine;
 
+            if (!IsFilesOver)
+            {
+                var fileName = textFileName[0];
+                if (File.Exists(fileName))
+                {
+                    using (StreamReader sr = new StreamReader(fileName))
+                    {
+                        resultLine = sr.ReadToEnd();
+                        textFileName.Remove(fileName);
+
+                        return resultLine;
+                    }
+                }
+            }
+
+            throw new NullReferenceException();
+        }
+
+        public bool IsFilesOver { get => textFileName.Count == 0; }
+    }
 
     public class LineStreamProcessing : StreamProcessing
     {
         private const int MAX_CHAR_ON_LINE = 150;
-        private string _allText;        
+        private string _allText =string.Empty;        
 
         public string SetText
         {
-            set => _allText = value;
+            set
+            {
+                if (_allText.Length == 0)
+                    _allText = value;
+                else
+                {
+                    _allText += value;
+                }
+            }
         }
 
         public string GetStreamProcessing()
@@ -58,7 +86,7 @@ namespace Lesson_2_4
 
             for(int i = 0; i < MAX_CHAR_ON_LINE; i++)
             {
-                if (_allText.Length < i)
+                if (_allText.Length > i)
                 {
                     resultMessage += _allText[i];
                 }
@@ -69,10 +97,13 @@ namespace Lesson_2_4
                 }
             }
 
-            _allText.Substring(lastChar);
+            _allText = _allText.Substring(lastChar);
 
             return resultMessage;
         }
+
+        public bool CanReturnStream { get => _allText.Length >= MAX_CHAR_ON_LINE; }
+
     }
 
     public class ConsoleDisplay : VisualDisplay
@@ -83,12 +114,49 @@ namespace Lesson_2_4
         }
     }
 
+    public class TextWriter
+    {
+        private ReceivingInformation _receivingInformation;
+        private StreamProcessing _streamProcessing;
+        private VisualDisplay _visualDisplay;
+
+        public TextWriter(ReceivingInformation receivingInformation, StreamProcessing streamProcessing, VisualDisplay visualDisplay)
+        {
+            _receivingInformation = receivingInformation;
+            _streamProcessing = streamProcessing;
+            _visualDisplay = visualDisplay;
+        } 
+
+        public void Update()
+        {
+            if (!_streamProcessing.CanReturnStream)
+            {
+                _streamProcessing.SetText =_receivingInformation.GetInformation();
+            }
+
+            _visualDisplay.PrinteInfo(_streamProcessing.GetStreamProcessing());
+
+            Thread.Sleep(1000);
+        }
+    }
+
 
     public class asdasd
     {
         public static void Main (string[] args)
         {
-            FolderTXTReceivingInformation ds = new FolderTXTReceivingInformation(@"C:\Users\ba3a2\Desktop\test\", "*.txt");
+            FolderTXTReceivingInformation _tXTReceiving = new FolderTXTReceivingInformation(@"C:\Users\ba3a2\Desktop\test\", "*.txt");
+            LineStreamProcessing lineStreamProcessing = new LineStreamProcessing();
+            ConsoleDisplay consoleDisplay = new ConsoleDisplay();
+
+            TextWriter textWriter = new TextWriter(_tXTReceiving, lineStreamProcessing, consoleDisplay);
+
+            textWriter.Update();
+            textWriter.Update();
+            textWriter.Update();
+            textWriter.Update();
+
+
         }
     }
 
