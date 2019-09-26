d3d0c93972bc75fb13a54c684c577527fb2589c7
Andru <ba3a2022@gmail.com>
2019-09-26 09:55:45 +0700

    - Create new Branch - Create class Product , and base base methods

--- Lesson_1/Lesson_1/Program.cs
+++ Lesson_1/Lesson_1/Program.cs
@@ -4,10 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson_1
+namespace Lesson_2_5
 {
-    class Program
+    public interface IProperty { }
+    public class Product
     {
+        public string Name { get; private set; }
+        public int Price { get; private set; }
 
+        public Product(string name, int price, params IProperty[] properties)
+        {
+            Name = name;
+            Price = price;
+            _properties = properties.ToList();
+        }
+
+        private List<IProperty> _properties = new List<IProperty>();
+
+        public IEnumerable<IProperty> GetPropertys { get => _properties; }
+
+        public bool ContainsProperties(IProperty property)
+        {
+           return _properties.Contains(property);
+        }
+
+        public void AddProperties(IProperty property)
+        {
+            if (!ContainsProperties(property))
+            {
+                _properties.Add(property);
+            }
+        }
+    }
+
+    public class Shop
+    {
+        private List<Product> _products = new List<Product>();
+
+        public IEnumerable<Product> GetPropertys { get => _products; }
     }
 }
