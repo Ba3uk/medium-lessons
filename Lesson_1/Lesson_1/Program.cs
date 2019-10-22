e0a61cc026da8496e7eb58f04c2f3dcc6c767ab5
Andru <ba3a2022@gmail.com>
2019-10-22 18:35:33 +0700

    create branch

--- Lesson_1/Lesson_1/Program.cs
+++ Lesson_1/Lesson_1/Program.cs
@@ -4,10 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson_1
+namespace Lesson_4_1
 {
-    class Program
+    public interface ILogger
     {
+        void WriteLog(string message);
+    }
 
+    public class ConsoleLogWriter : ILogger
+    {
+        public virtual void WriteLog(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    public class SecureLogWritter : ConsoleLogWriter
+    {
+        private ILogger _logger;
+        public virtual void WriteLog(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+        public class SecureConsoleLogWritter : ConsoleLogWriter
+    {
+        private DayOfWeek _targetDayWeek;
+
+        public SecureConsoleLogWritter(DayOfWeek dayOfWeek)
+        {
+            _targetDayWeek = dayOfWeek;
+        }
+
+        public override void WriteLog(string message)
+        {
+            if (DateTime.Now.DayOfWeek == _targetDayWeek)
+            {
+                base.WriteLog(message);
+            }
+        }
+    }
+
+    public class FileLogWriter : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
